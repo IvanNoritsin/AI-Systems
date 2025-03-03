cf484898f4395f79b17aa01c239768ad268566f9
Норицин Иван Алексеевич <defenderivan2015@gmail.com>
2025-03-03 20:18:10 +0700

    Lab 4 done

--- Lab 4/AIS_Lab_4.py
+++ Lab 4/AIS_Lab_4.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GridSearchCV, cross_val_score
+from sklearn import tree
 
 
 data_path = "https://archive.ics.uci.edu/ml/machine-learning-databases/glass/glass.data"
@@ -24,15 +26,21 @@ X_train, X_holdout, y_train, y_holdout = train_test_split(data[['RI', 'Na', 'Mg'
                                                           random_state=12,
                                                           stratify=data['Type'])
 
-tree = DecisionTreeClassifier(max_depth=5,
+tree_model = DecisionTreeClassifier(max_depth=5,
                               random_state=21,
                               max_features=2)
 
-tree.fit(X_train, y_train)
+tree_model.fit(X_train, y_train)
 
-tree_pred = tree.predict(X_holdout)
+tree_pred = tree_model.predict(X_holdout)
 accuracy = accuracy_score(y_holdout, tree_pred)
 print("Оценка точности классификатора методом hold-out:", accuracy)
+
+tree.export_graphviz(tree_model,
+                     feature_names=data[['RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe']].columns,
+                     class_names=[str(c) for c in data['Type'].unique()],
+                     out_file='glass_tree_1.dot',
+                     filled=True, rounded=True)
 
 # ---
 
@@ -40,10 +48,10 @@ d_list = list(range(1,20))
 cv_scores = []
 
 for d in d_list:
-    tree = DecisionTreeClassifier(max_depth=d,
+    tree_model = DecisionTreeClassifier(max_depth=d,
                                   random_state=21,
                                   max_features=2)
-    scores = cross_val_score(tree, 
+    scores = cross_val_score(tree_model, 
                              data[['RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe']],
                              data['Type'],
                              cv=9,
@@ -71,10 +79,10 @@ f_list = list(range(1, 10))
 cv_scores_features = []
 
 for f in f_list:
-    tree = DecisionTreeClassifier(max_depth=5,
+    tree_model = DecisionTreeClassifier(max_depth=5,
                                   random_state=21,
                                   max_features=f)
-    scores = cross_val_score(tree, 
+    scores = cross_val_score(tree_model, 
                              data[['RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe']],
                              data['Type'],
                              cv=9,
@@ -85,7 +93,7 @@ MSE_features = [1-x for x in cv_scores_features]
 
 plt.figure(2)
 plt.plot(f_list, MSE_features)
-plt.xlabel('Количество признаков разделения дерева (max_depth)');
+plt.xlabel('Количество признаков разделения дерева (max_features)');
 plt.ylabel('Ошибка классификации (MSE)')
 
 f_min = min(MSE_features)
@@ -105,11 +113,30 @@ tree_grid.fit(data[['RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe']], data[
 print('Лучшее сочетание параметров: ', tree_grid.best_params_)
 print('Лучшие баллы cross validation: ', tree_grid.best_score_)
 
-from sklearn import tree
-#import graphviz
-
 tree.export_graphviz(tree_grid.best_estimator_,
                                 feature_names=data[['RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe']].columns,
                                 class_names=[str(c) for c in data['Type'].unique()],
-                                out_file='glass_tree.dot',
+                                out_file='glass_tree_2.dot',
                                 filled=True, rounded=True)
+
+# ---
+
+X = data[['Ca', 'Na']]
+y = data['Type']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=17, stratify=y)
+tree_model = DecisionTreeClassifier(max_depth=12, random_state=21, max_features=3)
+tree_model.fit(X_train, y_train)
+
+x_min, x_max = X.iloc[:, 0].min() - 0.1, X.iloc[:, 0].max() + 0.1
+y_min, y_max = X.iloc[:, 1].min() - 0.1, X.iloc[:, 1].max() + 0.1
+xx, yy = np.meshgrid(np.linspace(x_min, x_max, 200), np.linspace(y_min, y_max, 200))
+
+Z = tree_model.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+
+plt.figure(3)
+plt.contourf(xx, yy, Z, alpha=0.3, cmap='Pastel1')
+sns.scatterplot(x=X_train.iloc[:, 0], y=X_train.iloc[:, 1], hue=y_train, palette='Dark2', edgecolor='k')
+plt.xlabel('Ca')
+plt.ylabel('Na')
